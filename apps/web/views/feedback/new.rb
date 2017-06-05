9560e76060b7d3890bf30549c8af737b03aebf47
Dmitry Trager <dmitry.trager@flatstack.com>
2017-06-05 13:36:18 +0300

    Fix form markup

--- apps/web/views/feedback/new.rb
+++ apps/web/views/feedback/new.rb
@@ -3,5 +3,28 @@ require "slim"
 module Web::Views::Feedback
   class New
     include Web::View
+
+    def form
+      form_for feedback, url: routes.feedback_path  do
+        div class: "form-inputs" do
+          label :name
+          text_field :name
+          br
+          label :email
+          text_field :email
+          br
+          label :phone
+          text_field :phone
+          br
+          label :message
+          text_field :message, as: :text
+          br
+        end
+
+        div class: "form-actions" do
+          submit "Submit"
+        end
+      end
+    end
   end
 end
